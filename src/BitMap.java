2303501f53ee939f6bfccab93e54654078242788
MatiasFlores <mafloresgu@gmail.com>
2022-10-10 20:52:06 -0300

    6

--- src/BitMap.java
+++ src/BitMap.java
@@ -35,22 +35,15 @@ public class BitMap {
     }
 
     public int Select(int j) {
+
         int cont=0;
-        int i=0;
         while(cont==j){
+            int i=0;
             if(p[i]==1){
                 cont++;
             }
             i++;
-
-
         }
-        if(cont<j){
-            return -1;
-        }else{
-            return i;
-        }
+        return cont;
     }
-
-
 }
--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        BitMap m = new BitMap(7);
-        m.on(5);
-        System.out.print(m.access(5));
-    }
-}
