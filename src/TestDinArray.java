2cf50b1f229d06c1056c12d6be9585b13116c139
smjoys <iefh00@gmai.com>
2022-10-14 22:43:09 -0300

    18

--- src/TestDinArray.java
+++ src/TestDinArray.java
@@ -10,20 +10,23 @@ public class TestDinArray {
         int c,x;
 
 
-        c = b.Access(0);
-        b.On(c);
-        s.Insertar(c);
-        b.Off(c);
 
+        b.On(3);
+        s.Insertar(64);
+        s.Insertar(-3);
+        b.Off(4);
 
+        System.out.print("las posiciones: ");
         for (int i = 0; i < 9; i++) {
             System.out.print("[" + i+ "]");
         }
         System.out.println();
+        System.out.print("los arreglos:  ");
         for (int i = 0; i < 9; i++) {
             System.out.print("[" + s.Mostrar(i)+ "]");
         }
         System.out.println();
+        System.out.print("los bits:       ");
         for (int j =0;j<9;j++){
             System.out.print("[" + b.Access(j) + "]");
         }
