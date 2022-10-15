44f857ce92e6fd902d908d1205f005d1005c9047
MatiasFlores <mafloresgu@gmail.com>
2022-10-14 23:32:37 -0300

    20

--- src/TestDinArray.java
+++ src/TestDinArray.java
@@ -31,10 +31,29 @@ public class TestDinArray {
         }
 
         System.out.println();
-        System.out.println(s.Esta(64));
+        System.out.println("En que posicion esta el 64:" + s.Esta(64));
+        System.out.println();
+        System.out.println("Elemento de el DinArray" +s.Size());
+        System.out.println();
+        System.out.println("Eliminando 64");
+        s.Eliminar(64);
 
-
-
+        System.out.println();
+        System.out.println();
+        System.out.print("las posiciones: ");
+        for (int i = 0; i < 9; i++) {
+            System.out.print("[" + i+ "]");
+        }
+        System.out.println();
+        System.out.print("los arreglos:  ");
+        for (int i = 0; i < 9; i++) {
+            System.out.print("[" + s.Mostrar(i)+ "]");
+        }
+        System.out.println();
+        System.out.print("los bits:       ");
+        for (int j =0;j<9;j++){
+            System.out.print("[" + b.Access(j) + "]");
+        }
     }
 
 
